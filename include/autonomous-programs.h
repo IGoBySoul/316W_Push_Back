1c6e9ae29fe5048da0df9316b322c58360a4b09c
Patrick Block <pblock1709@myhta.org>
2025-07-22 21:29:36 -1000

    set PIDDrive to universal

--- include/autonomous-programs.h
+++ include/autonomous-programs.h
@@ -1,34 +1,29 @@
 #include "pid-setup.h"
 
+PIDDrive drive;
+
 void autonomous1() { //redLeft
-    PIDDrive drive;
-    drive.driveTo(12);
+    
 }
 
 void autonomous2() { //redRight
-    PIDDrive drive;
+    
 }
 
 void autonomous3() { //blueLeft
-    PIDDrive drive;
 }
 
 void autonomous4() { //blueRight
-    PIDDrive drive;
 }
 
 void autonomous5() {  //skills
-    PIDDrive drive;
 }
 
 void autonomous6() {  //progSkills
-    PIDDrive drive;
 }
 
 void autonomous7() {  //test1
-    PIDDrive drive;
 }
 
 void autonomous8() {  //test2
-    PIDDrive drive;
 }
